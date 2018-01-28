17161b662ccd695dd6ae224b78ac1e5363a2b39a
Tejas Rathod <tejasr@outlook.com>
2018-01-27 22:15:58 -0800

    .

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.WindowsAzure.MediaServices.Client;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    //class Program
+    //{
+    //    // Read values from the App.config file.
+
+    //    private static readonly string _AADTenantDomain =
+    //        ConfigurationManager.AppSettings["AMSAADTenantDomain"];
+    //    private static readonly string _RESTAPIEndpoint =
+    //        ConfigurationManager.AppSettings["AMSRESTAPIEndpoint"];
+    //    private static readonly string _AMSClientId =
+    //        ConfigurationManager.AppSettings["AMSClientId"];
+    //    private static readonly string _AMSClientSecret =
+    //        ConfigurationManager.AppSettings["AMSClientSecret"];
+
+    //    private static CloudMediaContext _context = null;
+    //    static void Main(string[] args)
+    //    {
+    //        AzureAdTokenCredentials tokenCredentials =
+    //            new AzureAdTokenCredentials(_AADTenantDomain,
+    //                new AzureAdClientSymmetricKey(_AMSClientId, _AMSClientSecret),
+    //                AzureEnvironments.AzureCloudEnvironment);
+
+    //        var tokenProvider = new AzureAdTokenProvider(tokenCredentials);
+
+    //        _context = new CloudMediaContext(new Uri(_RESTAPIEndpoint), tokenProvider);
+
+    //        // List all available Media Processors
+    //        foreach (var mp in _context.MediaProcessors)
+    //        {
+    //            Console.WriteLine(mp.Name);
+    //        }
+
+    //        Console.ReadLine();
+    //    }
+    //}
+}
---
+++ ConsoleApp1/Thumbnail.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using Microsoft.WindowsAzure.MediaServices.Client;
+using System.Threading;
+
+namespace EncodeAndGenerateThumbnails
+{
+    class Program
+    {
+        // Read values from the App.config file.
+        private static readonly string _AADTenantDomain =
+        ConfigurationManager.AppSettings["AMSAADTenantDomain"];
+        private static readonly string _RESTAPIEndpoint =
+        ConfigurationManager.AppSettings["AMSRESTAPIEndpoint"];
+        private static readonly string _AMSClientId =
+        ConfigurationManager.AppSettings["AMSClientId"];
+        private static readonly string _AMSClientSecret =
+        ConfigurationManager.AppSettings["AMSClientSecret"];
+
+        private static CloudMediaContext _context = null;
+
+        private static readonly string _mediaFiles =
+        Path.GetFullPath(@"../..\Media");
+
+        private static readonly string _singleMP4File =
+            Path.Combine(_mediaFiles, @"TheSimpsons.mp4");
+
+        static void Main(string[] args)
+        {
+            AzureAdTokenCredentials tokenCredentials =
+                new AzureAdTokenCredentials(_AADTenantDomain,
+                    new AzureAdClientSymmetricKey(_AMSClientId, _AMSClientSecret),
+                    AzureEnvironments.AzureCloudEnvironment);
+
+            var tokenProvider = new AzureAdTokenProvider(tokenCredentials);
+
+            _context = new CloudMediaContext(new Uri(_RESTAPIEndpoint), tokenProvider);
+
+            // Get an uploaded asset.
+            var asset = _context.Assets.FirstOrDefault();
+
+            // Encode and generate the thumbnails.
+            EncodeToAdaptiveBitrateMP4Set(asset);
+
+            Console.ReadLine();
+        }
+
+        static public IAsset EncodeToAdaptiveBitrateMP4Set(IAsset asset)
+        {
+            // Declare a new job.
+            IJob job = _context.Jobs.Create("Media Encoder Standard Thumbnail Job");
+            // Get a media processor reference, and pass to it the name of the 
+            // processor to use for the specific task.
+            IMediaProcessor processor = GetLatestMediaProcessorByName("Media Encoder Standard");
+
+            // Load the XML (or JSON) from the local file.
+            string configuration = File.ReadAllText(@"..\..\ThumbnailPreset_JSON.json");
+
+            // Create a task
+            ITask task = job.Tasks.AddNew("Media Encoder Standard Thumbnail task",
+                    processor,
+                    configuration,
+                    TaskOptions.None);
+
+            // Specify the input asset to be encoded.
+            task.InputAssets.Add(asset);
+            // Add an output asset to contain the results of the job. 
+            // This output is specified as AssetCreationOptions.None, which 
+            // means the output asset is not encrypted. 
+            task.OutputAssets.AddNew("Output asset",
+                    AssetCreationOptions.None);
+
+            job.StateChanged += new EventHandler<JobStateChangedEventArgs>(JobStateChanged);
+            job.Submit();
+            job.GetExecutionProgressTask(CancellationToken.None).Wait();
+
+            return job.OutputMediaAssets[0];
+        }
+
+        private static void JobStateChanged(object sender, JobStateChangedEventArgs e)
+        {
+            Console.WriteLine("Job state changed event:");
+            Console.WriteLine("  Previous state: " + e.PreviousState);
+            Console.WriteLine("  Current state: " + e.CurrentState);
+            switch (e.CurrentState)
+            {
+                case JobState.Finished:
+                    Console.WriteLine();
+                    Console.WriteLine("Job is finished. Please wait while local tasks or downloads complete...");
+                    break;
+                case JobState.Canceling:
+                case JobState.Queued:
+                case JobState.Scheduled:
+                case JobState.Processing:
+                    Console.WriteLine("Please wait...\n");
+                    break;
+                case JobState.Canceled:
+                case JobState.Error:
+
+                    // Cast sender as a job.
+                    IJob job = (IJob)sender;
+
+                    // Display or log error details as needed.
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static IMediaProcessor GetLatestMediaProcessorByName(string mediaProcessorName)
+        {
+            var processor = _context.MediaProcessors.Where(p => p.Name == mediaProcessorName).
+            ToList().OrderBy(p => new Version(p.Version)).LastOrDefault();
+
+            if (processor == null)
+                throw new ArgumentException(string.Format("Unknown media processor", mediaProcessorName));
+
+            return processor;
+        }
+    }
+}
